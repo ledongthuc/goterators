6343de8547588d8058c2bfa33f64e0c6a35734c1
Hoang Tran <hoang.tran.work@gmail.com>
2022-03-29 17:17:23 +0700

    avg of an empty list should be zero

--- average.go
+++ average.go
@@ -2,6 +2,9 @@ package goterators
 
 // Average sum of all the source list divided by the total number of source list
 func Average[T Number](source []T) float64 {
+	if len(source) == 0 {
+		return 0
+	}
 	var sum T
 	for _, item := range source {
 		sum += item
