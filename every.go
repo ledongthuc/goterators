db42d5c7e5434e6b83ccc111451d00c8a33fb990
Thuc Le <ledongthuc@gmail.com>
2022-07-26 11:10:51 +0200

    Support any if it can

--- every.go
+++ every.go
@@ -1,7 +1,7 @@
 package goterators
 
 // Every checks all elements in the list with condition function. If it's yes return true; otherwise, return false.
-func Every[T comparable](source []T, conditionFunc func(item T) bool) bool {
+func Every[T any](source []T, conditionFunc func(item T) bool) bool {
 	for _, item := range source {
 		if !conditionFunc(item) {
 			return false
--- some.go
+++ some.go
@@ -1,7 +1,7 @@
 package goterators
 
 // Some checks at least one element in the list meet the condition; return true, or return false if all elements don't meet the condition.
-func Some[T comparable](source []T, conditionFunc func(item T) bool) bool {
+func Some[T any](source []T, conditionFunc func(item T) bool) bool {
 	for _, item := range source {
 		if conditionFunc(item) {
 			return true
